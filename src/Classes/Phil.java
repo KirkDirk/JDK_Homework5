ac471f57a31cecb049f02a6850a9c5ebdcbaf23c
KirkDirk <jackalex@ya.ru>
2024-11-25 23:44:49 +0500

    Add forks

--- src/Classes/Phil.java
+++ src/Classes/Phil.java
@@ -2,15 +2,22 @@ package Classes;
 
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
 
-public class Phil {
+public class Phil implements Runnable{
 
-    private boolean philsEatState = false;
-    private CountDownLatch cdlSnacks = new CountDownLatch(3);
+    private final int id;
+    private boolean philsEatState;
+    private CountDownLatch cdlSnacks;
+    private final Lock leftFork;
+    private final Lock rightFork;
 
-    public void setPhilsEatState(boolean philsEatState, CountDownLatch cdlSnacks){
+    public Phil(int id, boolean philsEatState, CountDownLatch cdlSnacks, Lock leftFork, Lock rightFork) {
+        this.id = id;
         this.philsEatState = philsEatState;
         this.cdlSnacks = cdlSnacks;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
     }
 
     @Override
@@ -18,9 +25,15 @@ public class Phil {
         return "Phil ";
     }
 
-    public void philSnack(){
+    public void philSnack(int id){
         cdlSnacks.countDown();
         setPhilsEatState(true);
+        System.out.println("Phil " + Integer.toString(id + 1) + " eat");
+    }
+
+    public void philThink(int id){
+        setPhilsEatState(false);
+        System.out.println("Phil " + Integer.toString(id + 1) + " think");
     }
 
     public void setPhilsEatState(boolean philsEatState) {
@@ -37,5 +50,28 @@ public class Phil {
 
     public boolean isPhilsEatState() {
         return philsEatState;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (isPhilsEatState()) {
+            System.out.println("Phil " + id + " is full");
+            philThink(this.id);
+        } else if (leftFork.tryLock() && rightFork.tryLock()) {
+            if (leftFork.tryLock()) {
+                System.out.println("Phil " + id + ": forks are busy");
+            }
+            philSnack(this.id);
+            leftFork.unlock();
+            rightFork.unlock();
+        } else {
+            philThink(this.id);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import Classes.Phil;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
@@ -21,62 +23,73 @@ public class Main {
         int countPhil = 5;
         int countSnacks = 3;
         CountDownLatch cdlCountSnacks = new CountDownLatch(countSnacks);
-        List<Phil> philList = new ArrayList<>();
+        //List<Phil> philList = new ArrayList<>();
+        Lock[] forks = new Lock[countPhil];
         for (int i = 0; i < countPhil; i++) {
-            Phil phil = new Phil();
-            phil.setCdlSnacks(cdlCountSnacks);
-            philList.add(phil);
+            //Phil phil = new Phil();
+            //phil.setCdlSnacks(cdlCountSnacks);
+            //philList.add(phil);
+            forks[i] = new ReentrantLock();
         }
 
-        Runnable snacking = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < countPhil; i++) {
-                    Phil currentPhil = philList.get(i);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (currentPhil.isPhilsEatState()){
-                        System.out.println(currentPhil.isPhilsEatState());
-                        currentPhil.setPhilsEatState(false);
-                        System.out.println(currentPhil + Integer.toString(i + 1) + " think");
-                        System.out.println(currentPhil.isPhilsEatState());
-                    } else {
-                        currentPhil.philSnack();
-                        System.out.println(currentPhil + Integer.toString(i + 1) + " eat");
-                    }
-                }
-            }
-        };
 
-        Runnable stateSnacking = new Runnable() {
-            @Override
-            public void run() {
-                while (!(cdlCountSnacks.getCount() == 0)) {
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("Phils have a snack:");
-                    for (int i = 0; i < countPhil; i++) {
-                        System.out.println("Phil " + Integer.toString(i + 1) +
-                                " had a snack " +
-                                (countSnacks - philList.get(i).getCdlSnacks().getCount()) +
-                                " times");
-                    }
-                }
+//        Runnable snacking = new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < countPhil; i++) {
+//                    Phil currentPhil = philList.get(i);
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    if (currentPhil.isPhilsEatState()){
+//                        currentPhil.philThink(i);
+//                    } else {
+//                        if (forks[i].tryLock() && forks[(i+countPhil-1)%countPhil].tryLock()){
+//                            currentPhil.philSnack(i);
+//                        }
+//
+//                    }
+//                }
+//            }
+//        };
 
-            }
-        };
+//        Runnable stateSnacking = new Runnable() {
+//            @Override
+//            public void run() {
+//                while (!(cdlCountSnacks.getCount() == 0)) {
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    System.out.println("Phils have a snack:");
+//                    for (int i = 0; i < countPhil; i++) {
+//                        System.out.println("Phil " + Integer.toString(i + 1) +
+//                                " had a snack " +
+//                                (countSnacks - philList.get(i).getCdlSnacks().getCount()) +
+//                                " times");
+//                    }
+//                }
+//
+//            }
+//        };
 
-        Thread snakingThread = new Thread(snacking);
-        snakingThread.start();
+        Thread[] snakingThread = new Thread[countPhil];
+        for (int i = 0; i < countPhil; i++) {
+            System.out.println("Phil " + (i +1) + " begins to eat");
+            snakingThread[i] = new Thread(new Phil(
+                    i,
+                    false,
+                    cdlCountSnacks,
+                    forks[i],
+                    forks[(i+countPhil-1)%countPhil]));
+            snakingThread[i].start();
+        }
 
-        Thread getStateSnacking = new Thread(stateSnacking);
-        getStateSnacking.start();
+//        Thread getStateSnacking = new Thread(stateSnacking);
+//        getStateSnacking.start();
 
         cdlCountSnacks.await();
 
